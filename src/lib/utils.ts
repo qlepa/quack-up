91e04a1748830d6d50f5fb2a5931e98b0b79669e
qlepa <t.klepacki@gmail.com>
2025-08-11 00:39:17 +0200

    Remove any type

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -18,7 +18,7 @@ export function isValidMessage(message: string): boolean {
   return message.trim().length > 0 && message.trim().length <= 1000;
 }
 
-export function debounce<T extends (...args: any[]) => any>(
+export function debounce<T extends (...args: unknown[]) => unknown>(
   func: T,
   wait: number
 ): (...args: Parameters<T>) => void {
